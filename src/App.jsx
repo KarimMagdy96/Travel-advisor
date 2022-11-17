79d17dd5837c77fdb013f5e7e03cf4be28679031
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-17 17:32:34 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { CssBaseline, Grid } from '@material-ui/core'
 import { useState } from 'react'
 import './App.css'
-
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 function App() {
   return (
-   <div>hello</div>
+   <>
+   <CssBaseline/>
+   </>
   )
 }
 
